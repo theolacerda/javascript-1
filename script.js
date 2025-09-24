cb1ab8aca33dd194d5c152e98c54d7a96ddc7981
theolacerda-create <theolacerda@gmail.com>
2025-09-24 15:12:39 -0300

    commit-3.0

--- script.js
+++ script.js
@@ -1,29 +1,29 @@
-let Qnt = 0;
-let opcao;
+ let Qnt = 0;
 
-do {
-    opcao = parseInt(prompt("Digite 1 para compra, 2 para venda e 0 para sair do programa"));
-
-    if (opcao === 1) {
+    function comprar() {
         let qtdCompra = parseInt(prompt("Quantas peças deseja comprar?"));
         if (qtdCompra > 0) {
             Qnt += qtdCompra;
-            alert("Compra realizada! Quantidade atual de peças: " + Qnt);
+            exibirResultado("Compra realizada! Quantidade atual de peças: " + Qnt);
         } else {
-            alert("Quantidade inválida!");
+            exibirResultado("Quantidade inválida!");
         }
-    } else if (opcao === 2) {
+    }
+
+    function vender() {
         let qtdVenda = parseInt(prompt("Quantas peças deseja vender?"));
         if (qtdVenda > 0 && qtdVenda <= Qnt) {
             Qnt -= qtdVenda;
-            alert("Venda realizada! Quantidade atual de peças: " + Qnt);
+            exibirResultado("Venda realizada! Quantidade atual de peças: " + Qnt);
         } else {
-            alert("Quantidade inválida ou insuficiente no estoque!");
+            exibirResultado("Quantidade inválida ou insuficiente no estoque!");
         }
-    } else if (opcao === 0) {
-        alert("Saindo do sistema...");
-    } else {
-        alert("Opção inválida!");
     }
 
-} while (opcao !== 0);
+    function sair() {
+        exibirResultado("Saida realizada");
+    }
+
+    function exibirResultado(mensagem) {
+        document.getElementById("resultado").innerText = mensagem;
+    }
