08bffe0b7e0d63da2d9d3a08a13d425af13c9c1e
Théo Lacerda <theolacerda@gmail.com>
2025-09-24 11:12:28 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,29 @@
-var Qnt= 0;
-var num = parseFloat(window.prompt("Digite 1 para compra, 2 para venda e 0 para sair do programa"));
-if(num===1){
-    Qnt++
-    var num = parseFloat(window.alert("Foi comprada mais uma peça, sua quantidade atual de peças é igual a: " +Qnt));
-}
+let Qnt = 0;
+let opcao;
 
+do {
+    opcao = parseInt(prompt("Digite 1 para compra, 2 para venda e 0 para sair do programa"));
 
+    if (opcao === 1) {
+        let qtdCompra = parseInt(prompt("Quantas peças deseja comprar?"));
+        if (qtdCompra > 0) {
+            Qnt += qtdCompra;
+            alert("Compra realizada! Quantidade atual de peças: " + Qnt);
+        } else {
+            alert("Quantidade inválida!");
+        }
+    } else if (opcao === 2) {
+        let qtdVenda = parseInt(prompt("Quantas peças deseja vender?"));
+        if (qtdVenda > 0 && qtdVenda <= Qnt) {
+            Qnt -= qtdVenda;
+            alert("Venda realizada! Quantidade atual de peças: " + Qnt);
+        } else {
+            alert("Quantidade inválida ou insuficiente no estoque!");
+        }
+    } else if (opcao === 0) {
+        alert("Saindo do sistema...");
+    } else {
+        alert("Opção inválida!");
+    }
+
+} while (opcao !== 0);
