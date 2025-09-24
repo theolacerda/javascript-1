0f39846b11512853f4ef7961c2d2d2da5b22104e
Théo Lacerda <theolacerda@gmail.com>
2025-09-24 09:36:45 -0300

    commit-1.0

---
+++ script.js
@@ -0,0 +1,8 @@
+var Qnt= 0;
+var num = parseFloat(window.prompt("Digite 1 para compra, 2 para venda e 0 para sair do programa"));
+if(num===1){
+    Qnt++
+    var num = parseFloat(window.alert("Foi comprada mais uma peça, sua quantidade atual de peças é igual a: " +Qnt));
+}
+
+
