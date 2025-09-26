b66ecf60533a2bef8f84158dfcc134389e4394e9
theolacerda-create <theolacerda@gmail.com>
2025-09-25 21:35:07 -0300

    uptade

--- Exercicios-js/Ex-1/script.js
+++ Exercicios-js/Ex-1/script.js
@@ -1,20 +1,35 @@
-alert('Você está entrando no exercício número 1');
+alert('Bom dia Isadora🎉🎉🎉');
 
 let resp = prompt('Digite Ok se você deseja continuar');
 
-while (resp !== "Ok") {
-    if (resp === "oK" || resp === "ok") {
-        alert('Escreve direito');
-    } else {
-        alert('Resposta errada');
-    }
+
+while (resp.toLowerCase() !== "ok") {
+    alert('Escreve direito😒');
     resp = prompt('Digite Ok se você deseja continuar');
 }
 
-if(resp===Ok){
-    alert('Meus parabéns');
-}
+alert('Meus parabéns');
+
 
 function exibirResultado(mensagem) {
     document.getElementById("resultado").innerText = mensagem;
 }
+
+
+const botaoFugitivo = document.getElementById("fugitivo");
+botaoFugitivo.addEventListener("mouseenter", () => {
+    const largura = window.innerWidth - botaoFugitivo.offsetWidth;
+    const altura = window.innerHeight - botaoFugitivo.offsetHeight;
+
+    const x = Math.random() * largura;
+    const y = Math.random() * altura;
+
+    botaoFugitivo.style.left = `${x}px`;
+    botaoFugitivo.style.top = `${y}px`;
+});
+
+
+const botaoTheo = document.getElementById("eu");
+botaoTheo.addEventListener("click", () => {
+    let resposta = alert("Que bom que você sabe");
+});
