8317ca88b8728b795c9c77ca89d62c709ee00f97
Théo Lacerda <theolacerda@gmail.com>
2025-10-16 09:23:50 -0300

    uptade

---
+++ Exercicios-js/Ex-3/script.js
@@ -0,0 +1,34 @@
+let num = 5;
+let chute = null;
+
+function teste() {
+    
+    chute = parseInt(document.getElementById("qtdCompra").value, 10);
+
+    if (isNaN(chute)) {
+        exibirResultado("Informe um número válido.");
+        return;
+    }
+
+    if (chute < 0 || chute > 10) {
+        exibirResultado("Informe um número entre 0 e 10.");
+        return;
+    }
+
+    if (chute === num) {
+        exibirResultado("Parabéns! Você acertou o número secreto.");
+    } else {
+        exibirResultado("Tente novamente");
+    }
+}
+
+function sair() {
+   
+    document.getElementById("qtdCompra").value = "";
+    chute = null;
+    exibirResultado("Saída realizada.");
+}
+
+function exibirResultado(mensagem) {
+    document.getElementById("resultado").innerText = mensagem;
+}
